1c36632d9dc87eb995c467ebebf4788b74eddbe4
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-02-27 11:04:29 +0530

    Create dff_tb.v

---
+++ PROB 5/2 falling_edge/dff_tb.v
@@ -0,0 +1,19 @@
+module dff_tb;  
+    reg clock;  
+    reg d;  
+   output q;
+ dff dut( .d(d),   
+                .clock (clock),  
+                .q (q));  
+  
+ initial begin  
+        clock = 0;  
+        d = 1;  
+       #15 d = 0;  
+       #30 d=1;
+       #45 d=0;
+        #100 $finish;
+	$display ("%0t, d=%0b q=%0b", $time, d, q);
+    end 
+  always #10 clock = ~clock;  
+endmodule  
